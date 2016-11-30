5332fa316c49eab4e2af413f299222c0c9282b32
redtwowolf <blacktwodog@gmail.com>
2016-11-30 16:04:20 +0800

    initial work

---
+++ Honeybee/Honeybee/MainViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MainViewController.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 30/11/2016.
+//  Copyright © 2016 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+let ScreenW = UIScreen.main.bounds.width
+let ScreenH = UIScreen.main.bounds.height
+
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        addNavLeftItem()
+        addNavRightItem()
+        
+        addAddBtn()
+    }
+
+    func addNavLeftItem() {
+        let nicknameBtn = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        nicknameBtn.backgroundColor = UIColor.red
+        nicknameBtn.setTitle("honeybee", for: .normal)
+        nicknameBtn.addTarget(self, action: #selector(leftItemAction), for: .touchUpInside)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: nicknameBtn)
+    }
+    func addNavRightItem() {
+        let btn = UIButton(type: .contactAdd)
+        btn.sizeToFit()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: btn)
+        btn.addTarget(self, action: #selector(rightItemAction), for: .touchUpInside)
+    }
+    
+    func addAddBtn() {
+        let addBtn = UIButton(type: .custom)
+        addBtn.setImage(UIImage(named: "add"), for: .normal)
+        addBtn.frame = CGRect(x: (ScreenW - 60) * 0.5, y: ScreenH - 80, width: 60, height: 60)
+        view.addSubview(addBtn)
+    }
+    
+    
+    func leftItemAction() {
+        let nav = UINavigationController(rootViewController: SetupViewController())
+        present(nav, animated: true, completion: nil)
+    }
+    func rightItemAction() {
+        navigationController?.pushViewController(CurveViewController(), animated: true)
+    }
+
+
+}
