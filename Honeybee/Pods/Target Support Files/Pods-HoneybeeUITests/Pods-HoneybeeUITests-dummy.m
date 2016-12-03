dcd05366df5ca0c6adcb8087bb3554926d97756e
CoderDB <coderdb@126.com>
2016-12-03 15:23:59 +0800

    install snapkit

---
+++ Honeybee/Pods/Target Support Files/Pods-HoneybeeUITests/Pods-HoneybeeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HoneybeeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HoneybeeUITests
+@end
---
+++ Honeybee/Pods/Target Support Files/Pods-HoneybeeUITests/Pods-HoneybeeUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_HoneybeeUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HoneybeeUITestsVersionString[];
+
