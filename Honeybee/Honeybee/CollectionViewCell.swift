208316710c8814e025b21b4fb8561fc30473c1ab
redtwowolf <blacktwodog@gmail.com>
2016-12-10 17:47:17 +0800

    use animation on keyboard view when swipe down in Card VC

---
+++ Honeybee/Honeybee/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 10/12/2016.
+//  Copyright © 2016 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
