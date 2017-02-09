ddaceadc51ae96ef3f51240187977f1d3fb0c719
redtwowolf <blacktwodog@gmail.com>
2017-02-09 17:48:31 +0800

    update file folders

---
+++ Honeybee/SectionCell.swift
@@ -0,0 +1,24 @@
+//
+//  SectionCell.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 09/02/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class SectionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
