f5272fa6b41ac80e7c711e5a999459b48e7c6eab
redtwowolf <dongbinghouy@gmail.com>
2017-02-11 21:11:12 +0800

    add RecordDetailVC

--- Honeybee/AppDelegate.swift
+++ Honeybee/AppDelegate.swift
@@ -18,10 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         let nav = UINavigationController(rootViewController: MainViewController())
-        nav.navigationBar.titleTextAttributes = [UIFontDescriptorTextStyleAttribute: UIColor.white]
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
-        
+    
         return true
     }
 
