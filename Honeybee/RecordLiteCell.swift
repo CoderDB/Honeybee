3e3c4c00e6287c1cdbe94740f3a940a86ceff36e
redtwowolf <blacktwodog@gmail.com>
2017-02-14 13:06:02 +0800

    update font size

--- Honeybee/RecordLiteCell.swift
+++ Honeybee/RecordLiteCell.swift
@@ -21,7 +21,7 @@ class RecordLiteCell: UITableViewCell {
     lazy var number: UILabel = {
         let label = UILabel()
         label.textAlignment = .right
-        label.font = HonybeeFont.h1_number
+        label.font = HonybeeFont.h2_number
         label.textColor = UIColor.black
         label.text = "98456.87"
         return label
--- Honeybee/Theme.swift
+++ Honeybee/Theme.swift
@@ -29,9 +29,9 @@ struct HonybeeFont {
     static let h6 = UIFont(descriptor: UIFontDescriptor(fontAttributes: [UIFontDescriptorFamilyAttribute: "PingFang SC", UIFontDescriptorFaceAttribute: "Semibold"]), size: 12)
     
     
-    static let h1_big = UIFont(name: "Silom", size: 48)
-    static let h1_number = UIFont(name: "Silom", size: 30)
-    static let h2_number = UIFont(name: "Silom", size: 18)
+    static let h1_number = UIFont(name: "Silom", size: 70)
+    static let h2_number = UIFont(name: "Silom", size: 30)
+    static let h3_number = UIFont(name: "Silom", size: 18)
 }
 
 struct HonybeePriority {
