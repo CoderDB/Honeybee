5a666e66ddccea7069679473a2a2213ae8054a9c
redtwowolf <dongbinghouy@gmail.com>
2017-02-14 17:13:22 +0800

    change Main UI

--- Honeybee/HeaderView.swift
+++ Honeybee/HeaderView.swift
@@ -13,51 +13,76 @@ class HeaderView: UIView {
     
     lazy var usernameBtn: UIButton = {
         let btn = UIButton(type: .custom)
-        btn.backgroundColor = UIColor.cyan
         btn.setTitle("二狗哥", for: .normal)
         btn.setTitleColor(UIColor.black, for: .normal)
         btn.titleLabel?.font = HonybeeFont.h1
         btn.contentHorizontalAlignment = .left
         return btn
     }()
+    lazy var userImgView: UIImageView = {
+        let imgView = UIImageView(image: UIImage(named: "avatar"))
+        imgView.layer.cornerRadius = 10
+        imgView.layer.masksToBounds = true
+        return imgView
+    }()
     
     lazy var filterBtn: UIButton = {
         let btn = UIButton(type: .custom)
-        btn.backgroundColor = UIColor.cyan
         btn.setTitle("分类", for: .normal)
         btn.setTitleColor(HonybeeColor.main, for: .normal)
         btn.titleLabel?.font = HonybeeFont.h4
         btn.contentHorizontalAlignment = .left
         return btn
     }()
+    
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = HonybeeColor.main
+        view.layer.cornerRadius = 10
+        return view
+    }()
 
     
+    
+    
     override init(frame: CGRect) {
-        super.init(frame: CGRect(x: 0, y: 0, width: ScreenW, height: 80))
+        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 205))
+        
         setupUI()
     }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     
     
     func setupUI() {
         usernameBtn.addTarget(self, action: #selector(usernameBtnClicked), for: .touchUpInside)
+        
         addSubview(usernameBtn)
+        addSubview(userImgView)
         addSubview(filterBtn)
+        addSubview(containerView)
         
         usernameBtn.snp.makeConstraints { (make) in
             make.left.equalTo(self).offset(10)
-            make.top.equalTo(self).offset(10)
-            make.width.equalTo(200)
-            make.height.equalTo(50)
+            make.top.equalTo(self)
+        }
+        userImgView.snp.makeConstraints { (make) in
+            make.left.equalTo(usernameBtn.snp.right).offset(10)
+            make.centerY.equalTo(usernameBtn)
+            make.height.width.equalTo(40)
         }
         filterBtn.snp.makeConstraints { (make) in
-            make.right.equalTo(self).offset(-10)
-            make.bottom.equalTo(usernameBtn)
-            make.width.equalTo(40)
-            make.height.equalTo(30)
+            make.right.bottom.equalTo(self).offset(-10)
         }
-    }
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        containerView.snp.makeConstraints { (make) in
+            make.left.equalTo(usernameBtn)
+            make.top.equalTo(usernameBtn.snp.bottom)
+            make.bottom.equalTo(self).offset(-10)
+            make.right.equalTo(filterBtn.snp.left).offset(-10).priority(HonybeePriority.low)
+        }
     }
     
     var usernameAction: (() -> ())?
