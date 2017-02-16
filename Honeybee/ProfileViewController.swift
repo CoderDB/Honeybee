cc1c6b86d09b189c4716749572a7e2109c16fc97
redtwowolf <dongbinghouy@gmail.com>
2017-02-16 21:14:40 +0800

    VCs inherit Base

--- Honeybee/ProfileViewController.swift
+++ Honeybee/ProfileViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ProfileViewController: BaseViewController {
     
     lazy var tableView = UITableView()
-
     var dataSource = [SetupItem]()
     
     override func viewDidLoad() {
@@ -19,14 +18,14 @@ class ProfileViewController: BaseViewController {
         
         setupNav(title: "个人信息")
         addTableView()
+        addRightNavItem()
+        
         let item1 = SetupItem(title: "昵称", subTitle: "MaryLee")
         let item2 = SetupImageItem(title: "头像", subTitle: "")
         
         dataSource.append(item1)
         dataSource.append(item2)
     }
-    
-    
     
     func addTableView() {
         view.addSubview(tableView)
@@ -46,7 +45,20 @@ class ProfileViewController: BaseViewController {
         tableView.register(SetupCell.self, forCellReuseIdentifier: "\(SetupCell.self)")
         tableView.tableHeaderView = ProfileHeader(height: 135)
     }
+    func addRightNavItem() {
+        let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 25))
+        btn.setTitle("分享", for: .normal)
+        btn.setTitleColor(HonybeeColor.main, for: .normal)
+        btn.titleLabel?.font = HonybeeFont.h4
+        btn.addTarget(self, action: #selector(rightBarButtonItemClicked), for: .touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: btn)
+        
+    }
+    func rightBarButtonItemClicked() {
+        
+    }
 }
+
 
 // MARK: UITableViewDataSource
 extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
