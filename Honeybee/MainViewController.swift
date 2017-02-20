c3e2c6a704461bd0a7a40b74c486d8b683502ac5
redtwowolf <blacktwodog@gmail.com>
2017-02-20 20:28:11 +0800

    update popover view in main

--- Honeybee/MainViewController.swift
+++ Honeybee/MainViewController.swift
@@ -15,54 +15,44 @@ let ScreenH = UIScreen.main.bounds.height
 
 class MainViewController: UIViewController {
 
-    lazy var cardVC: CardViewController = CardViewController()
-    lazy var customPresentationController: HBPresentationController = HBPresentationController(presentedViewController: self.cardVC, presenting: self)
+    var cardVC: CardViewController!
+    var customPresentationController: HBPresentationController!
     
     let tableView = UITableView(frame: .zero, style: .grouped)
     
-    var dest: UIViewController? = nil
-    
+    lazy var destVC: UIViewController = {
+        let vc = UIViewController()
+        vc.modalPresentationStyle = .popover
+        vc.preferredContentSize = CGSize(width: 70, height: 60)
+        vc.view.backgroundColor = UIColor.white
+        return vc
+    }()
     lazy var topBtn: UIButton = {
-        let btn = UIButton()
+        let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 70, height: 30))
         btn.setTitle("仅支出", for: .normal)
         btn.setTitleColor(HonybeeColor.main, for: .normal)
         btn.titleLabel?.font = HonybeeFont.h5
         return btn
     }()
     lazy var botBtn: UIButton = {
-        let btn = UIButton()
+        let btn = UIButton(frame: CGRect(x: 0, y: 30, width: 70, height: 30))
         btn.setTitle("仅收入", for: .normal)
         btn.setTitleColor(HonybeeColor.main, for: .normal)
         btn.titleLabel?.font = HonybeeFont.h5
         return btn
     }()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         automaticallyAdjustsScrollViewInsets = false
         
+        cardVC = CardViewController()
+        customPresentationController = HBPresentationController(presentedViewController: cardVC, presenting: self)
         cardVC.transitioningDelegate = customPresentationController
 
-    
-        let destVC = UIViewController()
-        topBtn.frame = CGRect(x: 0, y: 0, width: 70, height: 30)
-        botBtn.frame = CGRect(x: 0, y: 30, width: 70, height: 30)
-        topBtn.addTarget(self, action: #selector(topBtnClicked), for: .touchUpInside)
-        destVC.view.addSubview(topBtn)
-        destVC.view.addSubview(botBtn)
-        destVC.view.backgroundColor = UIColor.white
-        destVC.modalPresentationStyle = .popover
-        destVC.preferredContentSize = CGSize(width: 70, height: 60)
-        
-        dest = destVC
-        
         addTableView()
         addAddBtn()
-    }
-    func topBtnClicked() {
-        print("___+++++____")
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -71,31 +61,42 @@ class MainViewController: UIViewController {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
-    
+}
+
+// MARK: UIPopoverPresentationControllerDelegate
+extension MainViewController: UIPopoverPresentationControllerDelegate {
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+}
+// MARK: Popover
+extension MainViewController {
     func popView(btn: UIButton) {
-        guard let popoverVC = dest!.popoverPresentationController else {
+        guard let popoverVC = destVC.popoverPresentationController else {
             return
         }
+        destVC.view.addSubview(topBtn)
+        destVC.view.addSubview(botBtn)
+        topBtn.addTarget(self, action: #selector(topBtnClicked), for: .touchUpInside)
+        botBtn.addTarget(self, action: #selector(botBtnClicked), for: .touchUpInside)
         popoverVC.backgroundColor = UIColor.white
         popoverVC.delegate = self
         popoverVC.sourceView = btn
         popoverVC.sourceRect = btn.bounds
         popoverVC.permittedArrowDirections = .up
-        
-        present(dest!, animated: true, completion: nil)
+        present(destVC, animated: true, completion: nil)
     }
-}
-
-extension MainViewController: UIPopoverPresentationControllerDelegate {
-    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
-        return .none
+    func topBtnClicked() {
+        print("___top____")
+    }
+    func botBtnClicked() {
+        print("___bot____")
     }
 }
 
 
 // MARK: UI
 extension MainViewController {
-    
     func addTableView() {
         tableView.backgroundColor = UIColor.white
         view.addSubview(tableView)
