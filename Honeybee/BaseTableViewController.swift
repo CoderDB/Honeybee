2f09b2379b32d23d799b56a546d57480ce623c92
redtwowolf <blacktwodog@gmail.com>
2017-02-22 19:11:35 +0800

    add BaseTableViewController

---
+++ Honeybee/BaseTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  BaseTableViewController.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 22/02/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableViewController: BaseViewController {
+
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.separatorStyle = .none
+        tv.showsVerticalScrollIndicator = false
+        tv.showsHorizontalScrollIndicator = false
+        tv.dataSource = self
+        tv.delegate = self
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.top.equalTo(view).offset(64)
+            make.left.right.bottom.equalTo(view)
+        }
+    }
+}
+
+extension BaseTableViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
