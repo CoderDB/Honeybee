41aadd2e31a66906cd3b27e9ffd05e7801e6f535
redtwowolf <blacktwodog@gmail.com>
2017-02-22 19:48:54 +0800

    add BarCell

---
+++ Honeybee/BarHeader.swift
@@ -0,0 +1,149 @@
+//
+//  BarHeader.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 22/02/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class BarHeader: UIView {
+
+    convenience init(height: CGFloat) {
+        self.init(frame: CGRect(x: 0, y: 0, width: 0, height: height))
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var barView: BarChartView = {
+        let barView = BarChartView()
+        return barView
+    }()
+    
+    func createData(numbers: [Double]) -> BarChartData {
+        var dataEntries: [BarChartDataEntry] = []
+        for i in 0..<months.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: unitsSold[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let barChartDataSet = BarChartDataSet(values: dataEntries, label: nil)
+        
+        //        barChartData.barWidth = 1.0
+        
+        barChartDataSet.drawValuesEnabled = false //不显示条形柱的值
+        //        barChartDataSet.barBorderColor = UIColor.black //条形柱边框颜色
+        //        barChartDataSet.barBorderWidth = 5 //条形柱边框宽度
+        //        barChartDataSet.barShadowColor = UIColor.yellow
+        
+        //        barChartDataSet.label = "每日消费" //条形主下方指示说明
+        
+        barChartDataSet.colors = [UIColor.white.withAlphaComponent(0.5)] //条形柱颜色
+       
+        return BarChartData(dataSet: barChartDataSet)
+    }
+    
+    
+    var months: [Int] = Array(1..<32)
+    let unitsSold = [4000.0, 8090.0, 4756.45, 8923.0, 1879, 4000.0, 8090.0, 4756.45, 8923.0, 1879, 4000.0, 8090.0, 4756.45, 8923.0, 1879, 8090.0, 4756.45, 8923.0, 1879, 8090.0, 4756.45, 8923.0, 1879, 8090.0, 4756.45, 8923.0, 1879, 8090.0, 4756.45, 8923.0, 1879]
+    func setupUI() {
+        barView = BarChartView(frame: CGRect(x: 0, y: 0, width: ScreenW, height: 200))
+        addSubview(barView)
+        
+        var dataEntries: [BarChartDataEntry] = []
+        for i in 0..<months.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: unitsSold[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let barChartDataSet = BarChartDataSet(values: dataEntries, label: nil)
+        let barChartData = BarChartData(dataSet: barChartDataSet)
+        
+        //        barChartData.barWidth = 1.0
+        
+        barChartDataSet.drawValuesEnabled = false //不显示条形柱的值
+        //        barChartDataSet.barBorderColor = UIColor.black //条形柱边框颜色
+        //        barChartDataSet.barBorderWidth = 5 //条形柱边框宽度
+        //        barChartDataSet.barShadowColor = UIColor.yellow
+        
+        barChartDataSet.colors = [UIColor.white.withAlphaComponent(0.5)] //条形柱颜色
+        
+        //        barChartDataSet.label = "每日消费" //条形主下方指示说明
+        
+        // legend
+        //        barView.legend.drawInside = false //指示块位置
+        //        barView.legend.direction = .rightToLeft
+        barView.legend.form = .none
+        
+        // offset
+        barView.extraBottomOffset = -10
+        //        barView.extraLeftOffset = 10
+        
+        barView.noDataText = "请稍等"
+        barView.data = barChartData
+        barView.chartDescription?.text = ""
+        
+        // xAxis
+        barView.xAxis.labelTextColor = UIColor.white
+        barView.xAxis.valueFormatter = IndexAxisValueFormatter(values: months.map{"\($0)"}) //x轴label值的显示样式
+        barView.xAxis.drawAxisLineEnabled = false //不显示x轴
+        barView.xAxis.drawGridLinesEnabled = false // 不显示竖格线
+        barView.xAxis.labelPosition = .bottom
+        
+        // leftAxis
+        barView.leftAxis.labelTextColor = UIColor.white
+        barView.leftAxis.drawGridLinesEnabled = false  //当leftAxis和rightAxis的drawGridLinesEnabled都为false时，横格线才不会显示
+        barView.leftAxis.drawAxisLineEnabled = false
+        //        barView.leftAxis.valueFormatter = IndexAxisValueFormatter(values: unitsSold)
+        //        barView.leftAxis.drawZeroLineEnabled = false // 如果x轴不显示，那么y轴的0线当然不会显示，所以这句可以不要
+        //        barView.leftAxis.drawTopYLabelEntryEnabled = false //左侧y轴的最大值label不显示
+        
+        // rightAxis
+        barView.rightAxis.drawGridLinesEnabled = false
+        barView.rightAxis.drawLabelsEnabled = false //右侧y轴不显示label
+        barView.rightAxis.drawAxisLineEnabled = false //右侧y轴不显示
+        
+        // x, y轴双击都不缩放
+        barView.scaleXEnabled = false
+        barView.scaleYEnabled = false
+        
+        barView.isUserInteractionEnabled = false
+        
+    }
+    
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        context.saveGState()
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        let startColor = UIColor.red
+        let startComponents = startColor.cgColor.components!
+        let endColor = UIColor.orange
+        let endComponents = endColor.cgColor.components!
+        
+        let colorComponents = startComponents + endComponents
+        let location: [CGFloat] = [0, 1]
+        let gradient = CGGradient(colorSpace: colorSpace, colorComponents: colorComponents, locations: location, count: 2)
+        
+        let startPoint = CGPoint(x: 0, y: rect.height)
+        let endPoint = CGPoint(x: rect.width, y: 0)
+        
+        context.drawLinearGradient(gradient!, start: startPoint, end: endPoint, options: .drawsBeforeStartLocation)
+        
+        context.restoreGState()
+    }
+
+
+}
--- Honeybee/BarViewController.swift
+++ Honeybee/BarViewController.swift
@@ -11,46 +11,32 @@ import UIKit
 class BarViewController: BaseTableViewController {
     
     var dataSource = [SetupItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupNav(title: "衣")
-        
-        addTableView()
-        
-        let item1 = SetupArrowItem(title: "衣", subTitle: "10%")
-        let item2 = SetupArrowItem(title: "食", subTitle: "30%")
-        let item3 = SetupArrowItem(title: "住", subTitle: "30%")
-        let item4 = SetupArrowItem(title: "行", subTitle: "30%")
+        let item1 = SetupArrowItem(title: "衣", subTitle: "-28510")
+        let item2 = SetupArrowItem(title: "食", subTitle: "-30")
+        let item3 = SetupArrowItem(title: "住", subTitle: "-1096.56")
+        let item4 = SetupArrowItem(title: "行", subTitle: "-7782.30")
         dataSource.append(item1)
         dataSource.append(item2)
         dataSource.append(item3)
         dataSource.append(item4)
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
-    }
-}
-
-// MARK: UI
-extension BarViewController {
-    func addTableView() {
-        tableView.delegate = self
         
-        tableView.rowHeight = 60
-        tableView.register(PieCell.self, forCellReuseIdentifier: "\(PieCell.self)")
+        tableView.register(BarCell.self, forCellReuseIdentifier: "\(BarCell.self)")
+        tableView.tableHeaderView = BarHeader(height: 170)
     }
 }
 
 
 // MARK: UITableViewDataSource
-extension BarViewController: UITableViewDelegate {
+extension BarViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataSource.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "\(PieCell.self)") as! PieCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(BarCell.self)") as! BarCell
         cell.item = dataSource[indexPath.row]
         return cell
     }
