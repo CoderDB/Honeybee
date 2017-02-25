2b4b8ddfc68e273930fc2cd4400466b9b58f70a7
redtwowolf <dongbinghouy@gmail.com>
2017-02-25 20:35:35 +0800

    fixed errors after change nav bg image

---
+++ Honeybee/CardCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCollectionCell.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 25/02/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionCell: UICollectionViewCell {
+    
+}
