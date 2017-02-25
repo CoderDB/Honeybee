f2c675f258f5905224d82cdd85b558828cf7beae
redtwowolf <blacktwodog@gmail.com>
2017-02-25 20:53:51 +0800

    fixed inherit BaseCell error

--- Honeybee/RecordDetailCell.swift
+++ Honeybee/RecordDetailCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class RecordDetailCell: UITableViewCell {
+class RecordDetailCell: BaseTableViewCell {
     
     lazy var mainTitleLabel: UILabel = {
         let label = UILabel()
@@ -31,31 +31,7 @@ class RecordDetailCell: UITableViewCell {
     }()
     
     
-    
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        layer.cornerRadius = HonybeeConstant.cornerRadius
-        layer.borderWidth = 1
-        setupUI()
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override var frame: CGRect {
-        didSet {
-            var newFrame = frame
-            newFrame.origin.x += 10
-            newFrame.origin.y += 20
-            newFrame.size = CGSize(width: frame.width-20, height: frame.height-10)
-            super.frame = newFrame
-        }
-    }
-    
-    func setupUI() {
+    override func setupUI() {
         contentView.addSubview(mainTitleLabel)
         contentView.addSubview(subTitleLabel)
         
--- Honeybee/SetupCell.swift
+++ Honeybee/SetupCell.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class SetupCell: UITableViewCell {
+class SetupCell: BaseTableViewCell {
     
     
     lazy var mainTitleLabel: UILabel = {
@@ -67,30 +67,7 @@ class SetupCell: UITableViewCell {
         }
     }
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
-        
-        layer.cornerRadius = HonybeeConstant.cornerRadius
-        layer.borderWidth = 1
-        
-        setupUI()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override var frame: CGRect {
-        didSet {
-            var newFrame = frame
-            newFrame.origin.x += 10
-            newFrame.origin.y += 20
-            newFrame.size = CGSize(width: frame.width-20, height: frame.height-10)
-            super.frame = newFrame
-        }
-    }
-    
-    func setupUI() {
+    override func setupUI() {
         
         contentView.addSubview(mainTitleLabel)
         contentView.addSubview(subTitleLabel)
