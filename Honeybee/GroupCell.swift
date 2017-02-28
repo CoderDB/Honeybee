4a62219042a3c7d66d5e5a7b0df3fc25a4c77317
redtwowolf <dongbinghouy@gmail.com>
2017-02-28 19:58:21 +0800

    remove SectionCell

--- Honeybee/GroupCell.swift
+++ Honeybee/GroupCell.swift
@@ -8,41 +8,51 @@
 
 import UIKit
 
-class GroupCell: UITableViewCell {
 
+protocol GroupCellDelegate: class {
+    func didSelected(model: Recorder)
+}
+
+
+class GroupCell: UITableViewCell {
+        
     lazy var dateLabel: UILabel = {
         let label = UILabel()
-        label.textAlignment = .left
         label.font = HonybeeFont.h4_number
-        label.textColor = UIColor.black
-        label.text = "2月10日"
         return label
     }()
     lazy var weekdayLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .right
         label.font = HonybeeFont.h7
-        label.textColor = UIColor.black
-        label.text = "星期二"
         return label
     }()
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.dataSource = self
+        tv.delegate = self
+        tv.isScrollEnabled = false
+        tv.rowHeight = 60
+        tv.backgroundColor = UIColor.white
+        tv.separatorStyle = .none
+        tv.register(RecordLiteCell.self)
+        return tv
+    }()
+    weak var delegate: GroupCellDelegate?
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+        selectionStyle = .none
         layer.cornerRadius = HonybeeConstant.cornerRadius
-        layer.borderWidth = 3
+        layer.borderWidth = 2
         
-        backgroundColor = UIColor.orange
-        
+        backgroundColor = UIColor.white
         setupUI()
         
     }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     override var frame: CGRect {
         didSet {
             var newFrame = frame
@@ -53,20 +63,48 @@ class GroupCell: UITableViewCell {
         }
     }
     
+    var dataSource: [Recorder]? {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     func setupUI() {
         contentView.addSubview(dateLabel)
         contentView.addSubview(weekdayLabel)
+        contentView.addSubview(tableView)
         
         dateLabel.snp.makeConstraints { (make) in
             make.left.top.equalTo(contentView).offset(10)
-            make.width.equalTo(100)
-            make.height.equalTo(30)
         }
         weekdayLabel.snp.makeConstraints { (make) in
             make.top.equalTo(contentView).offset(10)
             make.right.equalTo(contentView).offset(-10)
-            make.width.equalTo(40)
-            make.height.equalTo(20)
         }
+        tableView.snp.makeConstraints { (make) in
+            make.top.equalTo(dateLabel.snp.bottom)
+            make.left.equalTo(dateLabel)
+            make.right.equalTo(weekdayLabel)
+            make.bottom.equalTo(contentView.snp.bottom).offset(-10)
+        }
+    }
+}
+
+// MARK: UITableViewDataSource
+extension GroupCell: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource!.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(RecordLiteCell.self)") as! RecordLiteCell
+        let model = dataSource?[indexPath.row]
+        cell.model = model
+        
+        dateLabel.text = model?.date
+        weekdayLabel.text = model?.weekday
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.didSelected(model: dataSource![indexPath.row])
     }
 }
