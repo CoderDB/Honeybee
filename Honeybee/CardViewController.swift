f0027554c0ddffdcc99364732df53e5b421f91b7
redtwowolf <dongbinghouy@gmail.com>
2017-03-05 19:22:40 +0800

    change CardVC UI

--- Honeybee/CardViewController.swift
+++ Honeybee/CardViewController.swift
@@ -23,6 +23,9 @@ class CardViewController: BaseViewController {
         return keyboard
     }()
     var collectionView: UICollectionView!
+    
+    var dataSource = [Array(1...20), Array(1...20), Array(5...20), Array(5...20)]
+    var headerTitles = ["生活日常", "每天吃饭", "住", "车"]
     var lastOffsetY: CGFloat = 0
     
     lazy var currentSelectedImgView = UIImageView()
@@ -35,7 +38,7 @@ class CardViewController: BaseViewController {
     }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        automaticallyAdjustsScrollViewInsets = false
+//        automaticallyAdjustsScrollViewInsets = false
         setupNav(title: "记账")
         addResultView()
         addCollectionView()
@@ -50,38 +53,23 @@ class CardViewController: BaseViewController {
 // MARK: UI / Event
 extension CardViewController {
     func addResultView() {
-        let containerView = UIView(frame: CGRect(x: 10, y: 64, width: view.frame.width-20, height: 70))
-        containerView.layer.cornerRadius = HonybeeConstant.cornerRadius
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.black.cgColor
+        let containerView = CardHeader(frame: CGRect(x: 10, y: 64, width: view.frame.width-20, height: 70))
         view.addSubview(containerView)
-        containerView.addSubview(currentSelectedImgView)
-        containerView.addSubview(resultLabel)
-        currentSelectedImgView.snp.makeConstraints { (make) in
-            make.left.equalTo(containerView).offset(10)
-            make.centerY.equalTo(containerView)
-            make.width.height.equalTo(40)
-        }
-        resultLabel.snp.makeConstraints { (make) in
-            make.right.equalTo(containerView).offset(-10)
-            make.centerY.equalTo(containerView)
-            make.left.equalTo(currentSelectedImgView.snp.right).offset(10)
-        }
     }
     func addCollectionView() {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 10
         layout.minimumInteritemSpacing = 10
-        layout.sectionHeadersPinToVisibleBounds = true
-//        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 10, right: 10)
-        layout.headerReferenceSize = CGSize(width: view.frame.width, height: 50)
         layout.itemSize = CGSize(width: 45, height: 45)
+        layout.sectionHeadersPinToVisibleBounds = true
+        layout.headerReferenceSize = CGSize(width: view.frame.width, height: 50)
         layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 50, right: 10)
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = UIColor.white
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(CardCollectionCell.self, forCellWithReuseIdentifier: "\(CardCollectionCell.self)")
+        collectionView.register(IconManagerSectionHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "\(IconManagerSectionHeader.self)")
         view.addSubview(collectionView)
         collectionView.snp.makeConstraints { (make) in
             make.top.equalTo(view).offset(64+70)
@@ -102,26 +90,30 @@ extension CardViewController {
 // MARK: UICollectionViewDataSource
 extension CardViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        return dataSource.count
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 200
+        return dataSource[section].count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CardCollectionCell.self)", for: indexPath)
         cell.backgroundColor = UIColor.randomColor()
         return cell
     }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        currentSelectedImgView.backgroundColor = UIColor.randomColor()
-        navigationController?.pushViewController(IconManagerViewController(), animated: true)
-    }
 }
 
 
 // MARK: UICollectionViewDelegateFlowLayout
 extension CardViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "\(IconManagerSectionHeader.self)", for: indexPath) as! IconManagerSectionHeader
+        header.titleLabel.text = headerTitles[indexPath.section]
+        return header
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        currentSelectedImgView.backgroundColor = UIColor.randomColor()
+        navigationController?.pushViewController(IconManagerViewController(), animated: true)
+    }
 }
 
 
