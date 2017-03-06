200e997b85f55641e1bb4e05e134fd8910f47243
redtwowolf <dongbinghouy@gmail.com>
2017-03-06 21:20:05 +0800

    add category_color json

--- Honeybee/Extension.swift
+++ Honeybee/Extension.swift
@@ -69,14 +69,13 @@ extension UIImage {
     
     static func image(color: UIColor) -> UIImage {
         let rect = CGRect(x: 0, y: 0, width: 10, height: 10)
-        UIGraphicsBeginImageContext(rect.size);
-        let context = UIGraphicsGetCurrentContext();
-        context?.setFillColor(color.cgColor);
-        context?.fill(rect);
-        
-        let theImage = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return theImage!;
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let theImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return theImage!
     }
     
     func roundedCornerImage(cornerRadius radius: CGFloat) -> UIImage {
