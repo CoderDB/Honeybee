45fa1177c101bf34dd085ecec48f76aadf89775f
redtwowolf <dongbinghouy@gmail.com>
2017-03-10 18:23:31 +0800

    add DataSource

---
+++ Honeybee/DataSource.swift
@@ -0,0 +1,45 @@
+//
+//  DataSource.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 10/03/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class DataSource: NSObject {
+    var items: [Any] = []
+    
+    init(items: [Any]) {
+        self.items = items
+    }
+    func item(at indexPath: IndexPath) -> Any {
+        return items[indexPath.item]
+    }
+}
+
+extension DataSource: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension DataSource: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
--- Honeybee/KindDataSource.swift
+++ Honeybee/KindDataSource.swift
@@ -8,34 +8,10 @@
 
 import UIKit
 
-class KindDataSource: NSObject {
+class KindDataSource: DataSource {
     
-    var identifier: String
-    var items: [Any] = []
-//    var config: (UICollectionViewCell, Any) -> Void
-    
-    init(id: String, items: [Any]) {
-        self.identifier = id
-        self.items = items
-//        self.config = config
-    }
-    
-    func item(at idx: IndexPath) -> Any {
-        return items[idx.item]
-    }
-}
-
-extension KindDataSource: UICollectionViewDataSource {
-
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
-    }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
-//        config(cell, items[indexPath.item])
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(KindCell.self)", for: indexPath)
         if let cell = cell as? KindCell, let model = items[indexPath.item] as? HoneybeeKind {
             cell.config(model: model)
         }
