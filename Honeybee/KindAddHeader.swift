702132732a36625cad259c573814ad5aeae71b85
redtwowolf <dongbinghouy@gmail.com>
2017-03-10 15:31:13 +0800

    add Kind add item vc

--- Honeybee/KindAddHeader.swift
+++ Honeybee/KindAddHeader.swift
@@ -9,24 +9,26 @@
 import UIKit
 
 class KindAddHeader: BaseHeader {
-
     
-    lazy var titleBtn: UIButton = {
-        let btn = UIButton()
-        btn.setTitle("衣食住行", for: .normal)
-        btn.setTitleColor(UIColor.white, for: .normal)
-        btn.titleLabel?.font = HB.Font.h1
-        return btn
-    }()
-    
+    lazy var titleLabel: UILabel = {
+        $0.font = HB.Font.h1
+        $0.textAlignment = .center
+        $0.textColor = UIColor.white
+        $0.layer.cornerRadius = HB.Constant.cornerRadius
+        $0.layer.masksToBounds = true
+        $0.backgroundColor = HB.Color.main
+        $0.text = "衣"
+        return $0
+    }(UILabel())
     
     override func setupUI() {
         super.setupUI()
         
-        containerView.backgroundColor = HB.Color.main
-        containerView.addSubview(titleBtn)
-        titleBtn.snp.makeConstraints { (make) in
-            make.center.equalTo(containerView)
+        rightBtn.setTitle("编辑", for: .normal)
+        containerView.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.edges.equalTo(containerView)
         }
     }
 }
---
+++ Honeybee/KindAddItemController.swift
@@ -0,0 +1,18 @@
+//
+//  KindAddItemController.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 10/03/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class KindAddItemController: BaseCollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
