0fe6911fa341f441ed590567eeebe18f13792014
redtwowolf <blacktwodog@gmail.com>
2017-03-13 22:43:57 +0800

    update SetupVC datasource

---
+++ Honeybee/SetupDataSource.swift
@@ -0,0 +1,19 @@
+//
+//  SetupDataSource.swift
+//  Honeybee
+//
+//  Created by nvicky on 13/03/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class SetupDataSource: DataSource {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(SetupCell.self)")
+        if let cell = cell as? SetupCell, let item = items[indexPath.row] as? SetupItem {
+            cell.item = item
+        }
+        return cell!
+    }
+}
