23b400fdb1230e55e506cb5b0ef8cf4fda4b0855
redtwowolf <blacktwodog@gmail.com>
2017-03-14 21:00:03 +0800

    update PieVC datasource

---
+++ Honeybee/PieDataSource.swift
@@ -0,0 +1,18 @@
+//
+//  PieDataSource.swift
+//  Honeybee
+//
+//  Created by nvicky on 14/03/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class PieDataSource: DataSource {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(PieCell.self)") as! PieCell
+        cell.item = items[indexPath.row] as! SetupItem
+        return cell
+    }
+    
+}
--- Honeybee/PieViewController.swift
+++ Honeybee/PieViewController.swift
@@ -11,8 +11,7 @@ import UIKit
 
 class PieViewController: BaseTableViewController {
 
-    
-    var dataSource = [SetupItem]()
+    var dataSource: PieDataSource!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,36 +21,32 @@ class PieViewController: BaseTableViewController {
         tableView.register(PieCell.self)
         tableView.tableHeaderView = PieHeader(height: 250)
         
-        let item1 = SetupArrowItem(title: "衣", subTitle: "10%")
-        let item2 = SetupArrowItem(title: "食", subTitle: "30%")
-        let item3 = SetupArrowItem(title: "住", subTitle: "30%")
-        let item4 = SetupArrowItem(title: "行", subTitle: "30%")
-        dataSource.append(item1)
-        dataSource.append(item2)
-        dataSource.append(item3)
-        dataSource.append(item4)
+        fetchData()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
+    func fetchData() {
+        let item1 = SetupArrowItem(title: "衣", subTitle: "10%")
+        let item2 = SetupArrowItem(title: "食", subTitle: "30%")
+        let item3 = SetupArrowItem(title: "住", subTitle: "30%")
+        let item4 = SetupArrowItem(title: "行", subTitle: "30%")
+        
+        dataSource = PieDataSource(items: [item1, item2, item3, item4])
+        tableView.dataSource = dataSource
+    }
 }
 
 
-// MARK: UITableViewDataSource
+// MARK: UITableViewDelegate
+
 extension PieViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource.count
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "\(PieCell.self)") as! PieCell
-        cell.item = dataSource[indexPath.row]
-        return cell
-    }
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let barVC = BarViewController()
-        barVC.setNavTitle(dataSource[indexPath.row].title)
-        navigationController?.pushViewController(barVC, animated: true)
+        if let item = dataSource.item(at: indexPath) as? SetupItem {
+            let barVC = BarViewController()
+            barVC.setNavTitle(item.title)
+            navigationController?.pushViewController(barVC, animated: true)
+        }
     }
 }
