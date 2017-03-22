be4a3addaa99b7caa754a25d568e159216c0e276
redtwowolf <dongbinghouy@gmail.com>
2017-03-22 15:23:11 +0800

    update KindAddVC datasource with reaml results

--- Honeybee/KindAddDataSource.swift
+++ Honeybee/KindAddDataSource.swift
@@ -7,22 +7,36 @@
 //
 
 import UIKit
+import RealmSwift
 
-class KindAddDataSource: DataSource {
+class KindAddDataSource: NSObject {
     
-    var colors: [UIColor]
-    init() {
-        self.colors = HBKindManager.manager.allColors()
-            .filter { !$0.isUsed }
-            .map { UIColor(hex: $0.name) }
-        super.init(items: colors)
+    
+    fileprivate let colors: [UIColor]
+    fileprivate let items: Results<HoneyBeeColor>
+    
+    override init() {
+        self.items = HBKindManager.manager.allColors()
+        self.colors = items.map { UIColor(hex: $0.name) }
+        super.init()
     }
     
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func item(at indexPath: IndexPath) -> HoneyBeeColor {
+        return items[indexPath.item]
+    }
+}
+
+extension KindAddDataSource: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return items.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(KindAddCell.self)", for: indexPath) as! KindAddCell
         cell.backgroundColor = colors[indexPath.item]
         return cell
     }
-    
-    
 }
