1c7cc802e63deb52437b9bcf43343cc6cb524dc6
redtwowolf <dongbinghouy@gmail.com>
2017-03-26 10:54:23 +0800

    add Login VC

---
+++ Honeybee/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 25/03/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
