cf8103b1be409cd012e17387ef6aee03724ce529
redtwowolf <blacktwodog@gmail.com>
2017-03-26 22:50:56 +0800

    update loginVC ui

--- Honeybee/LoginViewController.swift
+++ Honeybee/LoginViewController.swift
@@ -14,6 +14,20 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = HB.Color.main
+        view.addSubview(usernameTF)
+        view.addSubview(passwordTF)
+        
+        usernameTF.snp.makeConstraints { (make) in
+            make.bottom.equalTo(view.snp.centerY)
+            make.left.equalTo(view).offset(20)
+            make.right.equalTo(view).offset(-20)
+            make.height.equalTo(60)
+        }
+        passwordTF.snp.makeConstraints { (make) in
+            make.top.equalTo(usernameTF.snp.bottom).offset(5)
+            make.left.right.height.equalTo(usernameTF)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +35,16 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-
+    lazy var usernameTF: UITextField = {
+        let tf = UITextField()
+        tf.leftView = UIImageView(image: UIImage(named: "username_tf"))
+        tf.borderStyle = .roundedRect
+        return tf
+    }()
+    lazy var passwordTF: UITextField = {
+        let tf = UITextField()
+        tf.leftView = UIImageView(image: UIImage(named: "password_tf"))
+        tf.borderStyle = .roundedRect
+        return tf
+    }()
 }
