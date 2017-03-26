8f554f247d2fc1a6ca66df6f224c49b97505f40c
redtwowolf <dongbinghouy@gmail.com>
2017-03-26 23:41:13 +0800

    add textfield event in LoginVC

--- Honeybee/LoginViewController.swift
+++ Honeybee/LoginViewController.swift
@@ -18,6 +18,8 @@ class LoginViewController: UIViewController {
         view.addSubview(usernameTF)
         view.addSubview(passwordTF)
         
+        
+        
         usernameTF.snp.makeConstraints { (make) in
             make.bottom.equalTo(view.snp.centerY)
             make.left.equalTo(view).offset(20)
@@ -28,23 +30,58 @@ class LoginViewController: UIViewController {
             make.top.equalTo(usernameTF.snp.bottom).offset(5)
             make.left.right.height.equalTo(usernameTF)
         }
+        usernameTF.addTarget(self, action: #selector(usernameTFValueChanged(_:)), for: .editingChanged)
+        passwordTF.addTarget(self, action: #selector(passwordTFValueChanged(_:)), for: .editingChanged)
+//        usernameTF.applyRoundCorners(corners: [.topLeft, .topRight], radius: 10)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func usernameTFValueChanged(_ textField: UITextField) {
+        print(textField.text)
+    }
+    func passwordTFValueChanged(_ textField: UITextField) {
+        print(textField.text)
     }
     
-    lazy var usernameTF: UITextField = {
-        let tf = UITextField()
+    lazy var usernameTF: HBTextField = {
+        let tf = HBTextField()
+        tf.leftViewMode = .always
         tf.leftView = UIImageView(image: UIImage(named: "username_tf"))
         tf.borderStyle = .roundedRect
+        tf.clearButtonMode = .always
+    
         return tf
     }()
-    lazy var passwordTF: UITextField = {
-        let tf = UITextField()
+    lazy var passwordTF: HBTextField = {
+        let tf = HBTextField()
+        tf.leftViewMode = .always
         tf.leftView = UIImageView(image: UIImage(named: "password_tf"))
+        tf.rightViewMode = .whileEditing
+        tf.rightView = UIImageView(image: UIImage(named: "password_tf"))
         tf.borderStyle = .roundedRect
+        tf.clearButtonMode = .always
         return tf
     }()
+}
+
+class HBTextField: UITextField {
+    
+    func applyRoundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.frame = self.bounds
+        shapeLayer.path = path.cgPath
+        self.layer.mask = shapeLayer
+    }
+    
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        var iconFrame = super.leftViewRect(forBounds: bounds)
+        iconFrame.origin.x += 10
+        return iconFrame
+    }
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        var iconFrame = super.rightViewRect(forBounds: bounds)
+        iconFrame.origin.x -= 10
+        return iconFrame
+    }
+    
+
 }
