444bf396e0c9036af65667ca50bc7b31e0ff59cd
redtwowolf <blacktwodog@gmail.com>
2017-03-31 21:35:59 +0800

    use protocol to instead class in PieDataSource

--- Honeybee/PieDataSource.swift
+++ Honeybee/PieDataSource.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class PieDataSource: DataSource {
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "\(PieCell.self)") as! PieCell
-        cell.item = items[indexPath.row] as! SetupItem
-        return cell
+protocol PieDataSourceProtocol {
+    associatedtype ItemType
+    var items: [ItemType] { get }
+    func item(at indexPath: IndexPath) -> ItemType
+}
+
+
+class PieDataSource: NSObject, PieDataSourceProtocol {
+    typealias ItemType = SetupItem
+    var items: [ItemType]
+    func item(at indexPath: IndexPath) -> ItemType {
+        return items[indexPath.row]
     }
     
+    init(items: [ItemType]) {
+        self.items = items
+        super.init()
+    }
+}
+
+extension PieDataSource: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "\(PieCell.self)") as! PieCell
+        cell.item = item(at: indexPath)
+        return cell
+    }
 }
