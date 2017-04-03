2cec4cb51263a82b3dede4d9df826a81221649e9
redtwowolf <blacktwodog@gmail.com>
2017-04-03 22:00:44 +0800

    update SuperRecorder model

--- Honeybee/RLMRecorderSuper.swift
+++ Honeybee/RLMRecorderSuper.swift
@@ -22,52 +22,44 @@ import ObjectMapper
 
 class RLMRecorderSuper: RLMModel {
     
-//    dynamic var id: String = NSUUID().uuidString
     
-//    override class func primaryKey() -> String? {
-//        return "id"
-//    }
-    
-    dynamic var style: String = "plain"
     dynamic var color: String = ""
     dynamic var name: String = ""
-    dynamic var totalPay: Int = 0
-    
-    dynamic var yearMonthDay: String = ""
-    
     var recorders = List<RLMRecorder>()
-    var year: String {
-        return yearMonthDay.components(separatedBy: "-")[0]
-    }
-    var month: String {
-        return yearMonthDay.components(separatedBy: "-")[1]
-    }
-    var yearMonth: String {
-        return year + "-" + month
-    }
+    
+//    dynamic var totalPay: Int = 0
+    
+//    dynamic var yearMonthDay: String = ""
+    
+//    var year: String {
+//        return yearMonthDay.components(separatedBy: "-")[0]
+//    }
+//    var month: String {
+//        return yearMonthDay.components(separatedBy: "-")[1]
+//    }
+//    var yearMonth: String {
+//        return year + "-" + month
+//    }
     
     
     override func mapping(map: Map) {
-        style <- map["style"]
         color <- map["color"]
         name <- map["name"]
-        totalPay <- map["totalPay"]
+//        totalPay <- map["totalPay"]
         
         if let json = map["recorders"].currentValue as? [[String: Any]] {
             for item in json {
                 if let model = Mapper<RLMRecorder>().map(JSON: item) {
-//                    totalPay += model.money
                     recorders.append(model)
                     try! Database.default.add(model: model)
                 }
             }
-            yearMonthDay = recorders[0].yearMonthDay
         }
         
     }
-    override static func ignoredProperties() -> [String] {
-        return ["year", "month"]
-    }
+//    override static func ignoredProperties() -> [String] {
+//        return ["year", "month"]
+//    }
 //    func append(_ model: RLMRecorder) {
 //        try! self.realm?.write {
 //            recorders.append(model)
