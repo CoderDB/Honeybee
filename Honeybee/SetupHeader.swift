dd36a778fc80fe6401d7c77ddca7e2b9224b1f3f
redtwowolf <blacktwodog@gmail.com>
2017-04-12 22:52:42 +0800

    add TouchId file

--- Honeybee/SetupHeader.swift
+++ Honeybee/SetupHeader.swift
@@ -38,19 +38,6 @@ class SetupHeader: BaseHeader {
     }()
     
     
-    
-//    convenience init(height: CGFloat) {
-//        self.init(frame: CGRect(x: 0, y: 0, width: 0, height: height))        
-//    }
-//    
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        setupUI()
-//    }
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-    
     override func setupUI() {
         super.setupUI()
         
