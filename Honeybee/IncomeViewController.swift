fb2f9a9f04c623dc0e472065dc2c8b05c909ad6b
redtwowolf <dongbinghouy@gmail.com>
2017-04-21 17:39:39 +0800

    add AddRecorderVC

---
+++ Honeybee/IncomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  IncomeViewController.swift
+//  Honeybee
+//
+//  Created by Dongbing Hou on 21/04/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+class IncomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.orange
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
