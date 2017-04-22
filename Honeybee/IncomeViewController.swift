865d22d6ff24541d64af28f677e00bf148335b18
redtwowolf <blacktwodog@gmail.com>
2017-04-22 10:37:17 +0800

    update IncomeVC UI

--- Honeybee/IncomeViewController.swift
+++ Honeybee/IncomeViewController.swift
@@ -8,29 +8,98 @@
 
 import UIKit
 
-class IncomeViewController: UIViewController {
+class IncomeViewController: BaseViewController {
 
+    lazy var salaryBtn: UIButton = {
+        let btn = UIButton()
+        btn.titleLabel?.font = HB.Font.h3
+        btn.layer.cornerRadius = HB.Constant.cornerRadius * 0.5
+        btn.backgroundColor = .green
+        btn.setTitle("工资", for: .normal)
+        return btn
+    }()
+    lazy var partTimeBtn: UIButton = {
+        let btn = UIButton()
+        btn.titleLabel?.font = HB.Font.h3
+        btn.layer.cornerRadius = HB.Constant.cornerRadius * 0.5
+        btn.backgroundColor = .orange
+        btn.setTitle("兼职", for: .normal)
+        return btn
+    }()
+    lazy var otherBtn: UIButton = {
+        let btn = UIButton()
+        btn.titleLabel?.font = HB.Font.h3
+        btn.layer.cornerRadius = HB.Constant.cornerRadius * 0.5
+        btn.backgroundColor = .gray
+        btn.setTitle("其他", for: .normal)
+        return btn
+    }()
+    lazy var addBtn: UIButton = {
+        let btn = UIButton()
+        btn.titleLabel?.font = HB.Font.h3
+        btn.layer.cornerRadius = HB.Constant.cornerRadius * 0.5
+        btn.backgroundColor = .green
+        btn.setTitle("+", for: .normal)
+        return btn
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor.orange
+//        automaticallyAdjustsScrollViewInsets = false
+//        let stackTop = UIStackView(arrangedSubviews: [salaryBtn, partTimeBtn])
+//        stackTop.alignment = .center
+//        stackTop.distribution = .fillEqually
+//        stackTop.axis = .horizontal
+//        
+//        let stackBottom = UIStackView(arrangedSubviews: [otherBtn, addBtn])
+//        stackBottom.alignment = .center
+//        stackBottom.distribution = .fillEqually
+//        stackBottom.axis = .horizontal
+//        
+//        let stackContainer = UIStackView(arrangedSubviews: [stackTop, stackBottom])
+//        stackContainer.alignment = .center
+//        stackContainer.distribution = .fillEqually
+//        stackContainer.axis = .vertical
+        
+//        view.addSubview(stackContainer)
+//        stackContainer.snp.makeConstraints { (make) in
+//            make.top.equalTo(view).offset(62+20)
+//            make.left.right.equalTo(view)
+//            make.height.equalTo(200)
+//        }
+        
+        setupUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupUI() {
+        view.addSubview(salaryBtn)
+        view.addSubview(partTimeBtn)
+        view.addSubview(otherBtn)
+        view.addSubview(addBtn)
+        
+        salaryBtn.snp.makeConstraints { (make) in
+            make.top.equalTo(view).offset(64+20)
+            make.right.equalTo(view.snp.centerX).offset(-20)
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
+        partTimeBtn.snp.makeConstraints { (make) in
+            make.left.equalTo(view.snp.centerX).offset(20)
+            make.top.height.width.equalTo(salaryBtn)
+        }
+        otherBtn.snp.makeConstraints { (make) in
+            make.top.equalTo(salaryBtn.snp.bottom).offset(10)
+            make.left.height.width.equalTo(salaryBtn)
+        }
+        addBtn.snp.makeConstraints { (make) in
+            make.left.height.width.equalTo(partTimeBtn)
+            make.top.equalTo(otherBtn)
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func createBtn(title: String) -> UIButton {
+        let btn = UIButton()
+        btn.setTitle(title, for: .normal)
+        return btn
     }
-    */
-
 }
