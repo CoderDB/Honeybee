bb875b048560e69e9391f87b3c85b5bac4d6decf
redtowwolf <blacktwodog@gmail.com>
2017-05-04 18:57:57 +0800

    add MainViewModel file

---
+++ Honeybee/MainViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewModel.swift
+//  Honeybee
+//
+//  Created by admin on 04/05/2017.
+//  Copyright © 2017 Dongbing Hou. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+
+class MainViewModel {
+
+//    let items: Driver<[RLMRecorder]>
+//    let indicatorViewAnimating: Driver<Bool>
+//    let loadError: Driver<Error>
+//    
+//    init() {
+//        
+//    }
+    
+}
